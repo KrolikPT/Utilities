8a574e8e74588bd1525818c0b8efbb780ee05ee2
Krolik <krolikpt@gmail.com>
2022-01-27 17:40:53 +0000

    Commit

--- file_creator.py
+++ file_creator.py
@@ -7,10 +7,6 @@ def permission():
     os.system("chmod +x %s.%s" % (FILENAME, EXTENSION))
 
 
-def filename():
-    FILENAME = input("# Filename (without extension): ")
-
-
 def file_creator():
     print("\n#####################################################")
     print("#################### CREATE FILE ####################")
@@ -24,32 +20,34 @@ def file_creator():
     print("#####################################################")
     option = input("# Option: ")
 
+    FILENAME = input("# Filename (without extension): ")
+
     if option == "1":
         
         EXTENSION = ".sh"
 
-        with open(FILENAME + ".sh", "w") as f:
+        with open(FILENAME + EXTENSION, "w") as f:
             f.write("#!/bin/bash\n")
 
     elif option == "2":
 
         EXTENSION = ".py"
         
-        with open(FILENAME + ".py", "w") as f:
+        with open(FILENAME + EXTENSION, "w") as f:
             f.write("#!/usr/bin/env python\n")
 
     elif option == "3":
 
         EXTENSION = ".py"
 
-        with open(FILENAME + ".py", "w") as f:
+        with open(FILENAME + EXTENSION, "w") as f:
             f.write("#!/usr/bin/env python3\n")
 
     elif option == "4":
 
         EXTENSION = ".txt"
         
-        with open(FILENAME + ".txt", "w") as f:
+        with open(FILENAME + EXTENSION, "w") as f:
             f.write("")
 
     elif option == "0":
