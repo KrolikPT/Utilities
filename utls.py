d9ca5a3e0133d8a6e4cac1f3821f8d43a7c4b884
Krolik <krolikpt@gmail.com>
2022-01-27 17:52:03 +0000

    Commit

--- utls.py
+++ utls.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import os
 import sys
-from Utilities.installation import uninstall
 
 ##########################################################################################
 # MODULES
