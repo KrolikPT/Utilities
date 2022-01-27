97d5a39599d61263e4c555482d618cc8a2c3a36f
Krolik <krolikpt@gmail.com>
2022-01-27 17:47:11 +0000

    Commit

--- utls.py
+++ utls.py
@@ -25,10 +25,13 @@ elif len(sys.argv) == 2 and sys.argv[1] == "-i":
 	os.system("sudo cp colors.py /bin/colors.py")
 	os.system("sudo cp file_creator.py /bin/file_creator.py")
 	os.system("sudo chmod +x /bin/utls")
+	print("Script installed!")
+	print("Now you can use only: utls [OPTION]")
 elif len(sys.argv) == 2 and sys.argv[1] == "-u":
 	os.system("sudo rm /bin/utls")
 	os.system("sudo rm /bin/colors.py")
 	os.system("sudo rm /bin/file_creator.py")
+	print("The script was removed!")
 elif len(sys.argv) == 2 and sys.argv[1] == "-tc":
 	colors()
 elif len(sys.argv) == 2 and sys.argv[1] == "-cf":
