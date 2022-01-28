46731b309485da80ffa2ac6aaddc123a7d02a519
Krolik <krolikpt@gmail.com>
2022-01-28 21:07:21 +0000

    Commit

--- modules/installation.py
+++ modules/installation.py
@@ -2,6 +2,7 @@ import os
 
 def install():
     os.system("sudo cp utls.py /bin/utls")
+    os.system("sudo mkdir /bin/modules")
     os.system("sudo cp modules/* /bin/modules/")
     os.system("sudo chmod +x /bin/utls")
     print("Script installed!")
